1e4d350067957369ddc2d3b08f00acee0238ce61
Noxalus <yapogproject@gmail.com>
2013-04-21 23:59:43 +0200

    Fixed a little problem.

--- src/Intranet/NewsBundle/Controller/FrontController.php
+++ src/Intranet/NewsBundle/Controller/FrontController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class FrontController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      * @Template()
      */
     public function indexAction()
