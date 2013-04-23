26152d8d3daf134d60b6ffee6f7e8f2df83d29b0
Jonathan JARRI <noxalus@gmail.com>
2013-04-23 19:06:55 +0200

    Created a new entity for ProjectBundle.

---
+++ src/Intranet/ProjectBundle/Entity/ProjectDeadline.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Intranet\ProjectBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ProjectDeadline
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class ProjectDeadline
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\Column(name="project", type="object")
+     */
+    private $project;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return ProjectDeadline
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set project
+     *
+     * @param \stdClass $project
+     * @return ProjectDeadline
+     */
+    public function setProject($project)
+    {
+        $this->project = $project;
+    
+        return $this;
+    }
+
+    /**
+     * Get project
+     *
+     * @return \stdClass 
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+}
