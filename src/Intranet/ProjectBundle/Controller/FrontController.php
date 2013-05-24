23973d9bc1a8d18696ecbd242901ff455920bc7b
Erika <moriceerika@gmail.com>
2013-05-24 14:51:24 +0200

    Projects improvement
    - confirmation when removal
    - notifications
    - error management

--- src/Intranet/ProjectBundle/Controller/FrontController.php
+++ src/Intranet/ProjectBundle/Controller/FrontController.php
@@ -5,6 +5,7 @@ namespace Intranet\ProjectBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 use Intranet\ProjectBundle\Entity\Project;
 use Intranet\ProjectBundle\Entity\ProjectGroup;
 use Intranet\ProjectBundle\Form\Type\ProjectType;
@@ -138,4 +139,100 @@ class FrontController extends Controller
         );
     }
     
+        
+    /**
+     * @Route("/projet/{id_project}/editer", name="edit_project")
+     * @Template()
+     * @Secure(roles="ROLE_TEACHER")
+     */
+    public function editAction($id_project)
+    {
+        $request = $this->get('request');
+        $session = $request->getSession();
+        
+        $project = $this->getDoctrine()
+                ->getRepository('IntranetProjectBundle:Project')
+                ->find($id_project);
+
+        if ($project)
+        {
+            $form = $this->createForm(new ProjectType(), $project);
+            if ($request->getMethod() == 'POST')
+            {
+                $form->bind($request);
+                if ($form->isValid())
+                {                      
+                    // On l'enregistre notre objet $project dans la base de données
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($project);
+                    $em->flush();
+
+                    $msg = 'Projet édité avec succès.';
+                    $session->getFlashBag()->add('success', $msg);
+
+                    return $this->redirect($this->generateUrl('projects'));
+                }
+            }
+            return array(
+                'form' => $form->createView(),
+            );
+        }
+        else
+        {
+            $error = 'Il semblerait que ce projet n\'existe pas dans la base de données. L\'édition est donc impossible.';
+            $session->getFlashBag()->add('error', $error);
+            return $this->redirect($this->generateUrl('projects'));
+        }
+    }
+
+    
+     /**
+     * @Route("/projet/{id_project}/supprimer", name="delete_project")
+     * @Template()
+     * @Secure(roles="ROLE_TEACHER")
+     */
+    public function deleteAction($id_project)
+    {
+        $request = $this->get('request');
+        $session = $request->getSession();
+        
+        $project = $this->getDoctrine()
+                ->getRepository('IntranetProjectBundle:Project')
+                ->find($id_project);
+
+        if ($project)
+        {
+            $form = $this->createFormBuilder()->getForm();
+
+            $request = $this->getRequest();
+            if ($request->getMethod() == 'POST')
+            {
+                $form->bind($request);
+
+                if ($form->isValid())
+                {
+                    $em = $this->getDoctrine()->getManager();
+                    $em->remove($project);
+                    $em->flush();
+               
+                    $error = 'Projet supprimé avec succès.';
+                    $session->getFlashBag()->add('success', $error);
+                    
+                    return $this->redirect($this->generateUrl('projects'));
+                }
+            }
+
+            return array(
+                'project' => $project,
+                'form'    => $form->createView()
+                );
+        }
+        else
+        {
+            $error = 'Il semblerait que ce projet n\'existe pas dans la base de données. Il n\'a donc pas pu être supprimé.';
+            $session->getFlashBag()->add('error', $error);
+        }
+        return $this->redirect($this->generateUrl('projects'));
+        
+    }
 }
